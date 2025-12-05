b1bef16e90bc06fbb57b35ee57eafd5f02e2516c
Valerii <valerapiliaev@gmail.com>
2025-12-05 17:04:58 +0200

    feat: Added send_telegram_message function to send messages in Telegram bot

--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+import requests
+from django.conf import settings
 
-# Create your views here.
+
+def send_telegram_message(chat_id: str, text: str):
+    url = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {"chat_id": chat_id, "text": text, "parse_mode": "HTML"}
+    try:
+        requests.post(url, json=payload, timeout=5).raise_for_status()
+    except Exception as e:
+        print("Telegram message failed:", e)
